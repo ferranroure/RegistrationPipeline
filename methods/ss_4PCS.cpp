cbcf8bb7d3735ad50cb7400b9cdd4b032427b8b4
Ferran <froure@eia.udg.edu>
2016-04-15 11:38:14 +0200

    ICP GKDtree wokrking

--- methods/ss_4PCS.cpp
+++ methods/ss_4PCS.cpp
@@ -24,7 +24,7 @@ void ss_4PCS::execute() {
 
 
     // Aquest thr seria % de punts aparellats mínims a partir del qual considerem que s'ha fet registre. Si és -1 s'agafa l'estimació d'overlap "overlap".
-    float thr = 0.1; // THRESHOLD FOR THE bestf
+    float thr = 0.15; // THRESHOLD FOR THE bestf
 
     // Número de punts que consideren. Més o menys, pq en fan un tractament random raro.
     int n_points = 1000;
