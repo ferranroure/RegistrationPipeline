38ddc222aaedebde639bb4baabba65553a83a8cb
froure@eia.udg.edu <Bxy23fpg>
2016-06-30 17:18:03 +0200

    Adding Super4PCS

---
+++ methods/ss_Super4PCS.cpp
@@ -0,0 +1,34 @@
+#include "ss_Super4PCS.h"
+
+ss_Super4PCS::ss_Super4PCS(){
+
+    data = NULL;
+}
+
+ss_Super4PCS::~ss_Super4PCS() {
+
+}
+
+void ss_Super4PCS::setData(Data *d) {
+
+    data = d;
+}
+
+void ss_Super4PCS::execute() {
+
+    // Our matcher.
+    Match4PCSOptions options;
+
+    // Set parameters.
+    cv::Mat mat = cv::Mat::eye(4, 4, CV_64F);
+    options.overlap_estimation = overlap;
+    options.sample_size = n_points;
+    options.max_normal_difference = norm_diff;
+    options.max_color_distance = max_color;
+    options.max_time_seconds = max_time_seconds;
+    options.delta = delta;
+    // Match and return the score (estimated overlap or the LCP).
+    float score = 0;
+
+    MatchSuper4PCS matcher(options);
+}
---
+++ methods/ss_Super4PCS.h
@@ -0,0 +1,23 @@
+#ifndef PIPELINE_SS_SUPER4PCS_H
+#define PIPELINE_SS_SUPER4PCS_H
+
+#include "../ISearchingStrategy.h"
+#include "../include/point.h"
+#include "../include/Converters/converter4PCS.h"
+#include <vector>
+#include "../external/Super4PCS/super4pcs/shared4pcs.h"
+
+
+class ss_Super4PCS : public ISearchingStrategy{
+
+public:
+
+    ss_Super4PCS();
+    ~ss_Super4PCS();
+
+    void setData(Data *d);
+    void execute();
+};
+
+
+#endif //PIPELINE_SS_SUPER4PCS_H
